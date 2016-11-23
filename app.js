ae139fa436b954a59f771ca24b3521316f1e9740
Jonathan <jportorreal77@gmail.com>
2016-11-23 15:14:13 -0500

    created a mongo db and endpoint to query books

--- app.js
+++ app.js
@@ -1,15 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
 var app = express();
+app.use(bodyParser.json());
+
+Genre = require ('./models/genre');
+Book = require('./models/genre');
 
 // connect to mongoose
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.send('Please use /api/books or /api');
+});
+
+app.get('/api/genres', function(req, res){
+    Genre.getGenres(function(err, genres){
+        if(err){ 
+            throw err;
+        }
+
+        console.log(genres);
+        res.json(genres);
+    });
+});
+
+app.get('/api/books', function(req, res){
+    Book.getBooks(function(err, books){
+        if(err){
+            throw err;
+        }
+        res.json(books);
+    });
+
+});
+
+app.get('/api/books/:_id', function(req, res){
+    Book.getBookById(req.params._id, function(err, book){
+        if(err){
+            throw err;
+        }
+        res.json(book);
+    });
+
+
+});
+
+app.post('/api/genres', function(req, res){
+    var genre = req.body;
+    Genre.addGenre(genre, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+
+
 });
 
 app.listen(3000);
