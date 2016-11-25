8f3a8c48c7accb11e3f4a4a3d9b59a75cfb038cc
Jonathan <jportorreal77@gmail.com>
2016-11-25 13:38:39 -0500

    refactored my scraper

--- models/bn-scraper.js
+++ models/bn-scraper.js
@@ -1,55 +1,66 @@
 const cheerio = require('cheerio');
 const request = require('request');
+const promise = require('promise');
 const jsonfile = require('jsonfile');
+const mongoose = require('mongoose');
 const file = 'barnesandnobles.json';
 const URL = 'http://www.barnesandnoble.com';
-const NEW_RELEASE = '/b/new-releases/_/N-1oyg';
+const NEW_RELEASE = '/b/new-releases/_/N-1oyg?Nrpp=40';
 
+Book = require('./book');
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
 // gets data on new releases  
 
-function newReleases() {
+function newReleases(){
+
     request(URL + NEW_RELEASE, function(err, res, html){
 
 
-        if(!err && res.statusCode == 200) {
-            var $ = cheerio.load(html);
-            $("div[class=product-info]").each(function(i, element){
-                var buy_url = $(this).find('p[class=product-info-title] a').attr('href');
-                scrapeUrl(URL + buy_url);
-            });
-        }
+	if(!err && res.statusCode == 200) {
+	    var $ = cheerio.load(html);
+	   return $("div[class=product-info]").each(function(i, element){
+		var buy_url = $(this).find('p[class=product-info-title] a').attr('href');
+		scrapeUrl(URL + buy_url);
+	    }).next(console.log("finsihed"));
+	}
     });
+
 }
 
 // scrapes book data 
 function scrapeUrl (url) { 
     request(url, (err, res, html) => {
-        if(!err && res.statusCode == 200){
-            var $ = cheerio.load(html);
-            var title = $("h1").html();
-            var author = $("span[itemprop=author]").html();
-            var publisher = $("div[id=ProductDetailsTab] dl :nth-child(4) a").html();
-            var pages = $("div[id=ProductDetailsTab] dl :nth-child(8)").html();
-            var img_url = "http:" + $("img[itemprop=image]").attr('src');
-            var description = $("div[id=truncatedOverview] b").html();
-            var price = $("p[class=price] span").html();
+	if(!err && res.statusCode == 200){
+	    var $ = cheerio.load(html);
+	    var title = $("h1").html();
+	    var author = $("span[itemprop=author]").html();
+	    var publisher = $("div[id=ProductDetailsTab] dl :nth-child(4) a").html();
+	    var pages = $("div[id=ProductDetailsTab] dl :nth-child(8)").html();
+	    var img_url = "http:" + $("img[itemprop=image]").attr('src');
+	    var description = $("div[id=truncatedOverview] b").html();
+	    var price = $("p[class=price] span").html();
 
 
-            var bookdata = {
-                title: title,
-                author: author,
-                publisher: publisher,
-                pages: pages,
-                img_url: img_url,
-                description: description,
-                price: price
-            };
-            
-            jsonfile.writeFile(file, bookdata, {flag: 'a'}, function(err){
-               if(err) throw err;
-            })
-        }
+	    var bookdata = {
+		title: title,
+		author: author,
+		publisher: publisher,
+		pages: pages,
+		img_url: img_url,
+		description: description,
+		price: price
+	    };
+
+            console.log("updating new releases...");
+	    Book.addBook(bookdata);
+	}
     });
 }
+
+function closedb(){
+ db.close();
+}
+
 
 newReleases();
